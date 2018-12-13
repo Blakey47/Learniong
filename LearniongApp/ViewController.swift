a6ff94c872f3c927b3924ee23b4dfc96cdfb33e1
Darragh Blake <darragh@ua-c02xh67vjg5j.local>
2018-12-13 14:34:07 +0000

    Initial Commit

---
+++ LearniongApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearniongApp
+//
+//  Created by Darragh Blake on 13/12/2018.
+//  Copyright © 2018 Urban Airship. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
